22a240cc9bc3bc930a5a70e0c294b773fa7c4929
PRO X <bredalisgautreaux@gmail.com>
2024-01-21 10:59:04 -0400

    Archivo modificado y uno agregado

--- RLM.py
+++ RLM.py
@@ -1,99 +1,60 @@
 
 # Librerias
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
 
-# Mostrar los numeros
-# float con 2 decimales
+class RegresionLinealMultiple:
+    def __init__(self):
+        self.modelo = LinearRegression()
+        self.coeficientes = None
+        self.intercepto = None
 
-pd.options.display.float_format = '{:.2f}'.format
+    def entrenar(self, X, y):
+        X_entrenamiento, X_prueba, y_entrenamiento, y_prueba = train_test_split(X, y, test_size = 0.2, random_state = 42)
+        self.modelo.fit(X_entrenamiento, y_entrenamiento)
 
-# Lectura de datos
+        # Almacenar coeficientes e intercepto para referencia futura
+        self.coeficientes = self.modelo.coef_
+        self.intercepto = self.modelo.intercept_
 
-url = 'https://datasets-humai.s3.amazonaws.com/datasets/properati_caba_2021.csv'
-df = pd.read_csv(url)
+        # Evaluar el rendimiento en el conjunto de prueba
+        y_prediccion = self.modelo.predict(X_prueba)
+        mse = mean_squared_error(y_prueba, y_prediccion)
+        r2 = r2_score(y_prueba, y_prediccion)
 
-# Definimos la semilla
+        print(f'MSE: {mse}')
+        print(f'R2: {r2}')
 
-SEMILLA = 1992
+    def predecir(self, X_nuevos):
+        return self.modelo.predict(X_nuevos)
 
-print(df.head())
+# Crear una instancia de la clase
+modelo_rlm = RegresionLinealMultiple()
 
-# Correlacion entre las
-# variables numericas
-
-print(df.corr())
-print(df.columns)
-
-# Grafica
-
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows = 1, ncols = 4, figsize = (20, 5))
-
-df.plot.scatter(x = 'rooms', y = 'price', ax = ax1)
-df.plot.scatter(x = 'bathrooms', y = 'price', ax = ax2)
-df.plot.scatter(x = 'surface_total', y = 'price', ax = ax3)
-df.plot.scatter(x = 'surface_covered', y = 'price', ax = ax4)
-
-plt.show()
+# Datos
+datos = pd.DataFrame({
+  'X1': [1, 2, 3, 4, 5],
+  'X2': [2, 4, 5, 4, 5],
+  'y': [3, 5, 7, 8, 9]
+})
 
 # Division de datos
 
-x = df.drop(['price'], axis = 1)
-y = df['price']
+X = datos[['X1', 'X2']]
+y = datos['y']
 
-print(x)
-print(y)
+# Entrenar el modelo
+modelo_rlm.entrenar(X, y)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state = SEMILLA)
+# Hacer predicciones
+X_nuevos = pd.DataFrame({
+  'X1': [6, 7],
+  'X2': [8, 9]
+})
 
-# Definicion de las variables predictorias
-
-variables_exogenas = ['surface_total', 'bathrooms']
-
-# Matriz de X
-
-x_train_modelo_sup_baños = x_train[variables_exogenas]
-
-print(x_train_modelo_sup_baños.head())
-
-# Modelo
-
-modelo = LinearRegression(fit_intercept = True)
-
-# Entrenamiento
-
-modelo.fit(x_train_modelo_sup_baños, y_train)
-
-coeficientes = modelo.coef_
-intercepto = modelo.intercept_
-
-beta_1, beta_2 = coeficientes[0], coeficientes[1]
-
-print(beta_1)
-print(beta_2)
-
-# Definimos una función para obtener los coeficientes en un dataframe
-def obtener_coeficientes(modelo, lista_variables):
-
-  '''Crea un dataframe con los coeficientes estimados de un modelo'''
-  # Creo la lista de nombres de variables
-  lista_variables = ['intercepto'] + lista_variables
-
-  # Intercepto
-  intercepto = modelo.intercept_
-
-  # Lista coeficientes excepto el intercepto
-  coeficientes = list(modelo.coef_)
-
-  # Lista completa coeficientes
-  lista_coeficientes = [intercepto] + coeficientes
-
-  return pd.DataFrame({'variable': lista_variables, 'coeficiente': lista_coeficientes})
-
-obtener_coeficientes(modelo, variables_exogenas)
+predicciones = modelo_rlm.predecir(X_nuevos)
+print(f'Predicciones: {predicciones}')
